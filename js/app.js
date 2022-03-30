50851be3a73064004e249c139806bf318c6a0caa
Derry <thanhhaivevo@gmail.com>
2022-03-30 10:41:58 +0700

    update 1

--- js/app.js
+++ js/app.js
@@ -3,5 +3,16 @@ $(document).ready(function () {
         $('.nav_ul li').removeClass('active');
         $(this).addClass('active');
     });
+
+    $('.btn_cart').click(function () {
+        $('.cart').hide();
+        $('.payment').css('display', "grid");
+
+    });
+    $('.return').click(function () {
+        $('.payment').hide();
+        $('.cart').css('display', "grid");
+
+    });
     return false;
 })
